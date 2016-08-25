2e6b01cc9229366250a4e1a6f174a169a3440816
Heiko Behrens <mail@HeikoBehrens.net>
2016-08-25 12:07:50 +0000

    fix to work with latest SDK + some cleanup

--- src/pkjs/app.js
+++ src/pkjs/app.js
@@ -1,5 +1,3 @@
-var rocky = require('rocky');
-
 var ws = new WebSocket('ws://localhost:8080', ['rocky-repl']);
 
 ws.onopen = function() {
@@ -7,14 +5,14 @@ ws.onopen = function() {
 };
 
 ws.onmessage = function (evt) {
-  rocky.postMessage(JSON.parse(evt.data));
+  Pebble.postMessage(JSON.parse(evt.data));
 };
 
 ws.onclose = function() {
   console.log("PKJS: Connection is closed..."); 
 };
 
-rocky.on('message', function(event) {
+Pebble.on('message', function(event) {
   // Get the message that was passed
   var message = event.data;
   ws.send(JSON.stringify(message));
--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,5 +1,4 @@
-(function() {
-  var _global = this;  // FIXME: how to do this in a nicer way?
+(function(global) {
   this.rocky = require('rocky');
   this.rocky.on('message', function(event) {
     var message = event.data;
@@ -9,21 +8,20 @@
       };
 
       try {
-        var result = eval.apply(_global, [message.c]);
+        var result = eval.apply(global, [message.c]);
         if (result === undefined) {
           // Save some bandwidth, don't include anything :)
         } else if (result === null) {
           response.r = 'null';
         } else {
-          // TODO: are there any other things that don't have .toString() ?
-          response.r = result.toString();
+          response.r = '' + result;
         }
       } catch (e) {
-        response.r = e.toString();
+        response.r = '' + e;
       }
-      rocky.postMessage(response);
+      this.rocky.postMessage(response);
     } else {
       console.log('Unknown message:' + message);
     }
   });
-})();
+})(this);
